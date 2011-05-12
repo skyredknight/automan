1c165ff5be1c3a015b45a35dec3b9c796f9b5c17
German Beltran <skyredknight@gmail.com>
2011-05-12 19:14:24 -0300

    creación de proyecto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Automan::Application.config.secret_token = '64930df3975a978965464d1926c744058a25f4c11c9e51fff04ac2e31ada05de39b182446057f6b4af369bd7f95a4e0490b4a03486abc6c3ce68d9328a0988e7'
